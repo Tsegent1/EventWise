764c0411b3da527e10471f7a557bab5dc30e31cf
Tsegent Tekilemariam <ttekilemariam1@gmail.com>
2025-12-04 22:03:42 -0600

    Initial project setup with Event model

---
+++ src/main/java/com/eventwise/model/Event.java
@@ -0,0 +1,10 @@
+package com.eventwise.model;
+import java.time.LocalDate;
+
+public class Event {
+    private long id;
+    private String name;
+    private LocalDate date;
+    private String location;
+    private int maxAttendees;
+}
